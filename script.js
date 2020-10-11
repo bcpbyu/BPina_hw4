6da8f6796bf24b25ecc325cffd146268056b39d8
Pina <brannoncpina@gmail.com>
2020-10-10 23:50:28 -0600

    added scores and storage. Everything works excpet timer

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 var starterBtn = document.querySelector("#starterBtn");
 var quizStart = document.querySelector("#quizStart");
+var scorePage = document.querySelector("#scorePage");
 var asker = document.querySelector("#asker");
+var highscores = document.querySelector("#highscores");
+var allNames = [];
+var allScores = [];
+var seeScore = document.querySelector("#seeScore");
+var initials = document.querySelector("#initials");
+var input = document.querySelector("#input");
+var goAgain = document.querySelector("#goAgain");
 var question = document.querySelector("#questions");
-var buttons = document.querySelector("#option");
 var option1 = document.querySelector("#option1");
 var option2 = document.querySelector("#option2");
 var option3 = document.querySelector("#option3");
 var option4 = document.querySelector("#option4");
 var options = [option1, option2, option3, option4];
+var disScore = document.querySelector("#score");
 var score = 0;
-var questionasker;
 var questionNum = 0;
 var test = {
     quiz1: {
@@ -51,6 +58,8 @@ function nextQuest() {
     }
     else {
         asker.style.display = "none";
+        highscores.style.display = "block";
+        disScore.innerHTML = score;
     }
     console.log(questionNum);
     console.log(score);
@@ -65,56 +74,58 @@ starterBtn.addEventListener("click", function () {
 
 option1.addEventListener("click", function () {
     if (questionNum == 3) {
-        score = score+5;
+        score = score + 5;
     }
     nextQuest();
-    
+
 });
 option2.addEventListener("click", function () {
     if (questionNum == 1) {
-        score = score+5;
+        score = score + 5;
     }
     nextQuest();
-    
+
 });
 option3.addEventListener("click", function () {
     nextQuest();
 });
 option4.addEventListener("click", function () {
     if (questionNum == 2) {
-        score = score+5;
+        score = score + 5;
     }
     nextQuest();
-    
+
 });
 
-// for (var i = 0; i < options.length; i++) {
-//     options[i].addEventListener("click", function () {
-//         if (questionNum == 1) {
-//             question.innerHTML = test.quiz2.question;
-//             option1.innerHTML = test.quiz2.options[0];
-//             option2.innerHTML = test.quiz2.options[1];
-//             option3.innerHTML = test.quiz2.options[2];
-//             option4.innerHTML = test.quiz2.options[3];
-//             questionNum++;
-//             if (i == test.quiz1.answer) {
-//                 score = score +5
-//             }
-//             console.log(score);
-//         }
-//         else if (questionNum == 2) {
-//             question.innerHTML = test.quiz3.question;
-//             option1.innerHTML = test.quiz3.options[0];
-//             option2.innerHTML = test.quiz3.options[1];
-//             option3.innerHTML = test.quiz3.options[2];
-//             option4.innerHTML = test.quiz3.options[3];
-//             questionNum++;
-//             if (i == test.quiz2.answer) {
-//                 score = score +5
-//             }
-//             console.log(score);
-//         }
+seeScore.addEventListener("click", function () {
+    highscores.style.display = "none";
+    scorePage.style.display = "block";
+    allNames = JSON.parse(localStorage.getItem("initials"));
+    allScores = JSON.parse(localStorage.getItem("scores"));
+    for (var i = 0; i < allScores.length; i++) {
+        var some = allNames[i];
+        var some2 = allScores[i];
+    
+        var pa = document.createElement("p");
+        pa.textContent = some + ": " + some2;
+        pa.setAttribute("class", "text-center");
+    
+        scoreList.appendChild(pa);
+      }
+});
 
-//     });
+input.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var inputText = initials.value.trim();
+    allNames = JSON.parse(localStorage.getItem("initials"));
+    allScores = JSON.parse(localStorage.getItem("scores"));
+    allNames.push(inputText);
+    allScores.push(score);
+    initials.value = "";
+    localStorage.setItem("initials", JSON.stringify(allNames));
+    localStorage.setItem("scores", JSON.stringify(allScores));
+});
 
-
+goAgain.addEventListener("click", function() {
+    location.reload();
+});
