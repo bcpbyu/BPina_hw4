4d9838330b0b98e45d92d6823993de14430e227c
Pina <brannoncpina@gmail.com>
2020-10-10 20:59:23 -0600

    added my questions and had them change with button clicks

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 var starterBtn = document.querySelector("#starterBtn");
-var questiony = document.querySelector("#asker");
+var quizStart = document.querySelector("#quizStart");
+var asker = document.querySelector("#asker");
+var question = document.querySelector("#questions");
+var option1 = document.querySelector("#option1");
+var option2 = document.querySelector("#option2");
+var option3 = document.querySelector("#option3");
+var option4 = document.querySelector("#option4");
+var options = [option1, option2, option3, option4];
 var questionasker;
-
+var questionNum = 1;
 
 
 var test = {
     quiz1: {
-        question: "What is anything multiplied by zero",
+        question: "What is anything multiplied by zero?",
         options: ["anything", "zero", "infinity", "I dunno"],
         answer: 1
     },
@@ -21,16 +28,39 @@ var test = {
         answer: 3
     }
 };
-console.log("java");
 
 
 
-starterBtn.addEventListener("click", function() {
-    questiony.classList.remove("jumbotron");
-    questiony.innerHTML = "";
-    questionAsker = questiony.appendChild(h1);
-    questionAsker.createAttribute("class");
-    questionAsker.innerHTML = test.quiz1.question;
 
+starterBtn.addEventListener("click", function () {
+    quizStart.style.display = "none";
+    asker.style.display = "block";
+    question.innerHTML = test.quiz1.question;
+    option1.innerHTML = test.quiz1.options[0];
+    option2.innerHTML = test.quiz1.options[1];
+    option3.innerHTML = test.quiz1.options[2];
+    option4.innerHTML = test.quiz1.options[3];
 });
+for (var i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", function () {
+        if (questionNum == 1) {
+            question.innerHTML = test.quiz2.question;
+            option1.innerHTML = test.quiz2.options[0];
+            option2.innerHTML = test.quiz2.options[1];
+            option3.innerHTML = test.quiz2.options[2];
+            option4.innerHTML = test.quiz2.options[3];
+            questionNum++;
+        }
+        else if (questionNum == 2) {
+            question.innerHTML = test.quiz3.question;
+            option1.innerHTML = test.quiz3.options[0];
+            option2.innerHTML = test.quiz3.options[1];
+            option3.innerHTML = test.quiz3.options[2];
+            option4.innerHTML = test.quiz3.options[3];
+            questionNum++;
+        }
+
+    });
+}
+
 
