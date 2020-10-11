038d90d2d2ab47b418ceaff1d2d10ce2efb4bc59
Pina <brannoncpina@gmail.com>
2020-10-10 19:42:49 -0600

    added quiz questions

---
+++ script.js
@@ -0,0 +1,30 @@
+var starterBtn = document.querySelector("#starterBtn");
+
+var test = {
+    quiz1: {
+        question: "What is anything multiplied by zero",
+        options: ["anything", "zero", "infinity", "I dunno"],
+        answer: 1
+    },
+    quiz2: {
+        question: "What is anything to the power of zero?",
+        options: ["zero", "anything", "not possible", "infinity"],
+        answer: 0
+    },
+    quiz3: {
+        question: "What is the derivative of sine?",
+        options: ["not possible", "I dunno", "negative sine", "cosine"],
+        answer: 3
+    }
+
+
+
+};
+
+
+
+starterBtn.addEventListener("click", function() {
+
+
+
+});
